32e9f9478a5ccb50a256755d6432de68a24049db
Albin_Mp <albinmpobo@gmail.com>
2025-05-12 20:57:13 +0200

    ajout config connexion

--- api/db/database.js
+++ api/db/database.js
@@ -8,7 +8,10 @@ import mongoose from "mongoose";
 
 
 const DBConnect=async ()=>{
-   mongoose.connect(process.env.URL_MONG0_DB_ATLAS);
+   mongoose.connect(process.env.URL_MONG0_DB_ATLAS)
+   .then(() => console.log("✅ Connecté à MongoDB Atlas"))
+   .catch((err) => console.error("❌ Erreur de connexion :", err));
+   ;
 }
 
 export default DBConnect;
